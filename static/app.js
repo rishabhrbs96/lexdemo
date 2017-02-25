234a5de06b201bd05944df687a3b83ad9d82f16c
Scott Umsted <scottumsted@gmail.com>
2017-02-25 09:20:24 -0600

    update styling, add bot box

--- static/app.js
+++ static/app.js
@@ -8,7 +8,7 @@ var app = {
         $('#message').keyup(this.publishMessage);
     },
     'channelListener': function(obj){
-        $('#box').append('<br>'+(''+obj.message).replace( /[<>]/g, '' ));
+        $('#box').append(''+app.formatMessage((''+obj.message).replace( /[<>]/g, '' ))+'<br/>');
         $('#box').scrollTop($('#box')[0].scrollHeight);
     },
     'publishMessage': function(e){
@@ -17,9 +17,16 @@ var app = {
             name = (name=='')?'some-dude':name;
             var sez = $('#message').val();
             sez = (sez=='')?'sez nothing':sez;
-            var message = '@'+name+' - '+sez;
+            var message = '@'+name+' '+sez;
             app.pubnub.publish({channel: app.channel, message: message,x : ($('#message').val(''))});
         }
+    },
+    'formatMessage': function(message){
+        var tokens = message.split(' ');
+        var handle = tokens[0];
+        tokens.splice(0,1);
+        var remaining = tokens.join(' ');
+        return "<span class='handle'>"+handle+"</span><span class='message'>"+remaining+"</span>"
     }
 };
 $(document).ready(app.init());
