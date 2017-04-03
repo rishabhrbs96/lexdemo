1b7e5a9dc861b15ea65838df662a2a239e2f6a4d
Scott Umsted <scottumsted@gmail.com>
2017-04-03 07:44:56 -0500

    tweaking

--- smsconnector/smsservices.py
+++ smsconnector/smsservices.py
@@ -34,7 +34,7 @@ def get_sms():
 @post('/sms')
 def post_sms():
     print("Processing sms request")
-    print("Sending to channel: %s" % pn_smsrequest_channel)
+    print("Sending to channel: %s" % pn_chatbot_channel)
     message = {
         'responseChannel': pn_smsresponse_channel,
         'user': request.forms.get('From'),
