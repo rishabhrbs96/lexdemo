f832fc3d88f37d19daea16b06fdfa6d12e3d84d6
Scott Umsted <scottumsted@gmail.com>
2017-04-04 06:25:34 -0500

    smsservices is standalone

--- smsconnector/smsservices.py
+++ smsconnector/smsservices.py
@@ -1,29 +1,27 @@
 import os
 from pprint import pprint
 
-from bottle import get, post, request
+from bottle import get, post, request, run
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from twilio import twiml
-from twilio.rest import TwilioRestClient
 
-from smsconnector.smsconnector import my_publish_callback
 
 pnconfig = PNConfiguration()
 pnconfig.publish_key = os.environ.get('pubnub_publish_key', None)
 pnconfig.subscribe_key = os.environ.get('pubnub_subscribe_key', None)
+pn = PubNub(pnconfig)
 
 pn_chatbot_channel = os.environ.get('pubnub_chatbot_channel', None)
 pn_smsresponse_channel = os.environ.get('pubnub_smsresponse_channel', None)
 
-pn = PubNub(pnconfig)
+HOST = '0.0.0.0'
+PORT = port = int(os.environ.get("PORT", 5000))
 
-twilio_on = True
-twilio_account_sid = os.environ.get('twilio_account_sid', None)
-twilio_auth_token = os.environ.get('twilio_auth_token', None)
-twilio_ani = os.environ.get('twilio_ani', None)
 
-client = TwilioRestClient(account=twilio_account_sid, token=twilio_auth_token)
+@get('/')
+def get_index():
+    return 'smsservices'
 
 
 @get('/sms')
@@ -44,10 +42,22 @@ def post_sms():
     pprint(message)
 
     # publish to pubnub so the chatbot can handle it
-    pn.publish().channel(pn_chatbot_channel).message(message).async(my_publish_callback)
+    pn.publish().channel(pn_chatbot_channel).message(message).async(chatbot_publish_callback)
 
     # return an empty twiml response so as not to send message
     # we will handle this asynchronously using our pubnub listener below
     twiml_response = twiml.Response()
     return str(twiml_response)
 
+
+# callback specified when publishing to channel
+def chatbot_publish_callback(envelope, status):
+    if not status.is_error():
+        print("message published successfully")
+        pass  # Message successfully published to specified chatroomChannel.
+    else:
+        print("message published failure")
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+
+if __name__ == '__main__':
+    run(host=HOST, port=PORT)
--- webapp.py
+++ webapp.py
@@ -2,12 +2,11 @@ import os
 from bottle import run, template, static_file, route, get, TEMPLATE_PATH
 
 import coolwebservices
-import smsconnector.smsservices
 
-TEMPLATE_PATH.insert(0, os.environ.get('template_path','./views'))
+TEMPLATE_PATH.insert(0, os.environ.get('template_path', './views'))
 
 HOST = '0.0.0.0'
-PORT = port=int(os.environ.get("PORT", 5000))
+PORT = port = int(os.environ.get("PORT", 5000))
 
 pubnub = {
     'pubnub_publish_key': os.environ.get('pubnub_publish_key', None),
@@ -28,5 +27,5 @@ def get_fredsez():
     return template('fredsez', **pubnub)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run(host=HOST, port=PORT)
