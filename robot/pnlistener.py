b4d7745465e28840c346ade7147bb5867cac2d2f
Scott Umsted <scottumsted@gmail.com>
2017-04-09 10:32:08 -0500

    set fast speed default

--- robot/pnlistener.py
+++ robot/pnlistener.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     time.sleep(30)
     print("initialize robot...")
     gopigo_client.servo(90)
-    gopigo_client.set_speed(MySubscribeCallback.SLOW_SPEED)
+    gopigo_client.set_speed(MySubscribeCallback.FAST_SPEED)
     print("starting pnlistener...")
     pubnub.add_listener(MySubscribeCallback())
     pubnub.subscribe().channels([pn_robot_channel]).execute()
